c5276774eafd9e8dfb568dbd706d4136001e3a3a
Jonathan Underwood <jonathan@jyunderwood.com>
2014-06-05 20:18:56 -0500

    Move Xcode and Sublime to apps folder

---
+++ apps/zsh/aliases.zsh
@@ -0,0 +1,12 @@
+alias ls='ls -lhF'
+
+# Ruby
+alias rake='noglob rake'
+alias bx='bundle exec'
+alias guard='bx guard -c'
+
+# Clean up
+alias rm.gems='gem list --no-version | xargs gem uninstall -aIx'
+alias rm.pyc='find . -type f -name "*.pyc" -exec rm {} \;'
+alias rm.esub='find . -type d -exec rmdir 2>/dev/null {} \;'
+alias rm.dss='find . -type f -name ".DS_Store" -exec rm {} \;'
