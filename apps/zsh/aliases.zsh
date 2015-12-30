19065c97756390bc8b8b8dbf4860cfef977a6de2
Jonathan Underwood <jonathan@jyunderwood.com>
2015-12-30 17:55:14 -0600

    Change sublime preferences

--- apps/zsh/aliases.zsh
+++ apps/zsh/aliases.zsh
@@ -6,6 +6,12 @@ alias rake='noglob rake'
 alias bx='bundle exec'
 alias guard='bx guard -c'
 
+# Swift
+alias xc.s='sudo xcode-select -s /Applications/Xcode.app/Contents/Developer'
+alias xc.b='sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer'
+alias xc.bo='xcrun launch-with-toolchain /Library/Developer/Toolchains/swift-latest.xctoolchain'
+alias xc.es='export PATH="/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:$PATH"'
+
 # Clean up
 alias rm.gems='gem list --no-version | xargs gem uninstall -aIx'
 alias rm.pyc='find . -type f -name "*.pyc" -exec rm {} \;'
