7a4de7336a2f0bbd6fe97a0fdc733d2daefa6a55
Jonathan Underwood <jonathan@jyunderwood.com>
2018-09-24 08:58:42 -0500

    Disable KeyRepeat change

--- init/macos.sh
+++ init/macos.sh
@@ -95,8 +95,8 @@ defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+# defaults write NSGlobalDomain KeyRepeat -int 1
+# defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 ###############################################################################
 # Screen                                                                      #
